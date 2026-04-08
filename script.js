b6318d54ce6e2cd6687a610d8704c90cd937feca
vansh shende <vanshshende804@gmail.com>
2026-04-08 23:05:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    document.getElementById("popup").classList.add("active");
+}
+
+function closePopup() {
+    document.getElementById("popup").classList.remove("active");
+}
